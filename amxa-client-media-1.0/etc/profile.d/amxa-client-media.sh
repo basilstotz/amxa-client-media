88bd6c33b8581d4562e2091195ca0ccfefdb759f
Basil Stotz <basil.stotz@gmail.com>
2017-08-21 17:19:33 +0200

    *

--- amxa-client-media-1.0/etc/profile.d/amxa-client-media.sh
+++ amxa-client-media-1.0/etc/profile.d/amxa-client-media.sh
@@ -1,60 +1,62 @@
-if test $USER = "root"; then
-    echo "not for root..."
-    exit 1
-fi
-#
+
+if ! test $USER = "root"; then
+
+
 #  mount all media to Medien
-#
+
 # to much resoources!
-#if ! test -d "$HOME/Medien"; then
-#    mkdir -p "$HOME/Medien"
-#fi
-#if test -d "$HOME/Medien"; then
-#   if mount|grep -q -v djmount; then    
-#       djmount "$HOME/Medien"
-#   fi
-#fi
+#    if ! test -d "$HOME/Medien"; then
+#       mkdir -p "$HOME/Medien"
+#    fi
+#    if test -d "$HOME/Medien"; then
+#       if mount|grep -q -v djmount; then    
+#          djmount "$HOME/Medien"
+#       fi
+#     fi
+
+
+
 #
 #  start local mediaserver
 #
-if ! test "$USER" = "guest"; then
-    rygel --replace --title="MediaExport:@REALNAME@" &
-else
-    rygel --replace --title="MediaExport:@PRETTY_HOSTNAME@" &
-fi
+
+    if ! test "$USER" = "guest"; then
+       rygel --replace --title="MediaExport:@REALNAME@" &
+    else
+       rygel --replace --title="MediaExport:@PRETTY_HOSTNAME@" &
+    fi
 #
 #  start local webserver (incl. avahi)
 #
-PORT="80"
-PFAD=".config/public_html"
+    PORT="80"
+    PFAD=".config/public_html"
 
-if ! test -d "$HOME/$PFAD"; then
-    mkdir -p "$HOME/$PFAD"
-fi
-
-mklink(){
-    if test -d $HOME/$1; then
-	if ! test -h $HOME/$PFAD/$1; then
-            ln -s $HOME/$1 $HOME/$PFAD/$1
-	fi
-   fi
-}
-
-mklink Videos
-mklink Dokumente
-mklink Musik
-mklink Bilder
-
-if echo $USER|grep -q "[G|g]uest.*"; then
-    NAME="%h"
-else
-    NAME=$USER
-fi
-
-if test -d "$HOME/.config/public_html"; then
-    if nmap -p $PORT localhost|grep -q -v $PORT; then
-        /usr/share/bin/amxa-webfs-service $NAME $PORT $HOME/$PFAD/ $USER
+    if ! test -d "$HOME/$PFAD"; then
+      mkdir -p "$HOME/$PFAD"
     fi
-fi
 
-       
+
+#    mklink(){
+#       if test -d $HOME/$1; then
+#	   if ! test -h $HOME/$PFAD/$1; then
+#             ln -s $HOME/$1 $HOME/$PFAD/$1
+#	   fi
+#       fi
+#    }
+
+#    mklink Videos
+#    mklink Dokumente
+#    mklink Musik
+#    mklink Bilder
+
+    if echo $USER|grep -q "[G|g]uest.*"; then
+       NAME="%h"
+    else
+       NAME=$USER
+    fi
+
+    if nmap -p $PORT localhost|grep -q -v $PORT; then
+       /usr/share/bin/amxa-webfs-service $NAME $PORT $HOME/$PFAD/ $USER
+    fi
+
+fi
--- amxa-client-media_1.0-9ubuntu5.dsc
+++ amxa-client-media_1.0-9ubuntu5.dsc
@@ -2,7 +2,7 @@ Format: 1.0
 Source: amxa-client-media
 Binary: amxa-client-media
 Architecture: all
-Version: 1.0-9ubuntu2
+Version: 1.0-9ubuntu5
 Maintainer: Basil Stotz <stotz@amxa.ch>
 Homepage: <insert the upstream URL, if relevant>
 Standards-Version: 3.9.8
@@ -10,8 +10,8 @@ Build-Depends: debhelper (>= 9)
 Package-List: 
  amxa-client-media deb unknown optional
 Checksums-Sha1: 
- 89b49532c94e874a16c592d6937b2410bf42387c 12705 amxa-client-media_1.0-9ubuntu2.tar.gz
+ e21b8d1b35881140fa1e5edd54eb5c995f3305ba 12750 amxa-client-media_1.0-9ubuntu5.tar.gz
 Checksums-Sha256: 
- d1f6a08360eebd240192ac51e9bce66d1cf4190f5eb33e04ccd61f5bc0bc5452 12705 amxa-client-media_1.0-9ubuntu2.tar.gz
+ 2ee696136f5dbeb7a7a84a022693e5df9a36eb40619062c861f75306ec0190f1 12750 amxa-client-media_1.0-9ubuntu5.tar.gz
 Files: 
- 262059fb73e3e58a263157b2448cd82c 12705 amxa-client-media_1.0-9ubuntu2.tar.gz
+ c35074aca3728aee266452c5426489d1 12750 amxa-client-media_1.0-9ubuntu5.tar.gz
