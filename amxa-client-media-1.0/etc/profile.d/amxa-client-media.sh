c5f3380c0e7486d10c87870a90ebca0591c1bd46
Basil Stotz <basil.stotz@gmail.com>
2018-05-19 12:36:29 +0200

    fixed runtimeerror

--- amxa-client-media-1.0/etc/profile.d/amxa-client-media.sh
+++ amxa-client-media-1.0/etc/profile.d/amxa-client-media.sh
@@ -1,15 +1,29 @@
 
 if ! test $USER = "root"; then
 
+    # Personal Webserver
+    PORT="80"
+    PUBLIC_HTML="public_html"
+    # Public Media Browser
+    MEDIEN="Medien"
+    # Puablich Onw Media
+    MEINEMEDIEN="MeineMedien"
+
+
+    if echo $USER|grep -q "[G|g]uest.*"; then
+         NAME="%h"
+         if ! test -d $HOME/.local/$PUBLIC_HTML; then 
+             mkdir -p $HOME/.local/$PUBLIC_HTML;
+         fi
+         if ! test -d $HOME/$MEDIEN; then mkdir -p $HOME/$MEDIEN;fi
+         if ! test -d $HOME/$MEINEMEDIEN; then mkdir -p $HOME/$MEINEMEDIEN;fi
+    else
+         NAME=$USER
+    fi
+
 
 #  mount all media to Medien
 
-# to much resoources!
-#    if ! test -d "$HOME/Medien"; then
-#       mkdir -p "$HOME/Medien"
-#    fi
-
-    MEDIEN="Medien"
     if test -d "$HOME/$MEDIEN"; then
        if mount|grep -q -v $HOME/$MEDIEN; then    
           djmount "$HOME/$MEDIEN"
@@ -22,35 +36,27 @@ if ! test $USER = "root"; then
 #  start local mediaserver
 #
 
-  if test -d $HOME/MeineMedien; then
-    if ! test "$USER" = "guest"; then
-       rygel --replace --title="MediaExport:@REALNAME@" &
-    else
-       rygel --replace --title="MediaExport:@PRETTY_HOSTNAME@" &
-    fi
-  if
+  if test -d $HOME/$MEINEMEDIEN; then
+      if ! test "$USER" = "guest"; then
+         rygel --replace --uris="$HOME/$MEINEMEDIEN" --title="MediaExport:@REALNAME@" &
+      else
+         rygel --replace --uris="$HOME/$MEINEMEDIEN" --title="MediaExport:@PRETTY_HOSTNAME@" &
+      fi
+  fi
 
 #
 #  start local webserver (incl. avahi)
 #
 
-    PORT="80"
-    PFAD="$HOME/.config/public_html"
+   if test -d $HOME/.local/$PUBLIC_HTML; then 
 
-    if test -d $PFAD; then 
-
-      if ! test -l "$HOME/public_html"; then
-        mkdir -p "$HOME/public_html"
+      if ! test -l $HOME/$PUBLIC_HTML; then
+         ln -s $HOME/.local/$PUBLIC_HTML $HOME/$PUBLIC_HTML
       fi
  
-      if echo $USER|grep -q "[G|g]uest.*"; then
-         NAME="%h"
-      else
-         NAME=$USER
-      fi
 
       if nmap -p $PORT localhost|grep -q -v $PORT; then
-         /usr/share/bin/amxa-webfs-service $NAME $PORT $PFAD/ $USER
+         /usr/share/bin/amxa-webfs-service $NAME $PORT $HOME/.local/$PUBLIC_HTML/ $USER
       fi
 
    fi     #pfad
